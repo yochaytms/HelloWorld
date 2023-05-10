1a01e074a3a4be4750248ab1950b2a812f4a2caa
haikarmi <haika@ac.sce.ac.il>
2023-05-10 22:24:42 +0300

    added hello world class to project.

---
+++ HelloWorld.cpp
@@ -0,0 +1,5 @@
+//
+// Created by חי כרמי on 10/05/2023.
+//
+
+#include "HelloWorld.h"
---
+++ HelloWorld.h
@@ -0,0 +1,19 @@
+//
+// Created by חי כרמי on 10/05/2023.
+//
+
+#ifndef HELLOWORLD_HELLOWORLD_H
+#define HELLOWORLD_HELLOWORLD_H
+
+#include <iostream>
+
+using namespace std;
+
+class HelloWorld {
+    int main(){
+        cout << "Hello world! "<< endl;
+    }
+};
+
+
+#endif //HELLOWORLD_HELLOWORLD_H
